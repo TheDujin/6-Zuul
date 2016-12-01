a35c12dff2069f04a017c830971a18109c04bd50
U-BSD\293359 <293359@SHS-10D8A831B.beaverton.k12.or.us>
2016-12-01 14:31:31 -0800

    Trying to figure out how maps work

--- Room.cpp
+++ Room.cpp
@@ -1,3 +1,6 @@
 #include "Room.h"
 #include <iostream>
 #include <string.h>
+
+using namespace std;
+
--- Room.h
+++ Room.h
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string.h>
 
+using namespace std;
+
+class Room {
+ public:
+  char* getName();
+
+ protected:
+  char* title;
+  map <int, Room> exitMap;
